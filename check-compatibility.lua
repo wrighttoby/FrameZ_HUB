bd1780cfa04496670b1d8d36cb5799ccd9c434c0
wrighttoby <89476497+wrighttoby@users.noreply.github.com>
2023-01-10 16:50:10 +0000

    Create check-compatibility.lua

---
+++ check-compatibility.lua
@@ -0,0 +1,5 @@
+if game.GameId == 61846006 or game.GameId == 12042441540 then
+	script.Parent.Text = "Script is Compatible With The Game!"
+else
+	script.Parent.Text = "Script is NOT Compatible.. Limited Access"
+end
